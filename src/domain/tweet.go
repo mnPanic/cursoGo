63bf75e50bd4dd3226179ff0a4db546c572c63d9
alguien <alguien@example.com>
2017-11-24 15:25:51 -0300

    Fixed uppercasing in tweet.go

--- src/domain/tweet.go
+++ src/domain/tweet.go
@@ -4,20 +4,20 @@ import "time"
 
 //Tweet is a tweet
 type Tweet struct {
-	user string
-	text string
-	date *time.Time
+	User string
+	Text string
+	Date *time.Time
 }
 
 //NewTweet creates a tweet
 func NewTweet(usr, txt string) *Tweet {
 	now := time.Now()
-	tw := Tweet{user: usr, text: txt, date: &now}
+	tw := Tweet{User: usr, Text: txt, Date: &now}
 	return &tw
 }
 
 //StringTweet returns a tweet as a formatted string
 func StringTweet(tw Tweet) string {
-	st := tw.user + ": " + tw.text + ", " + tw.date.String()
+	st := tw.User + ": " + tw.Text + ", " + tw.Date.String()
 	return st
 }
